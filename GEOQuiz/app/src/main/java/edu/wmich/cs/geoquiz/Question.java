78f0a729aa6bc63eacc1063e67d0793babe64dfc
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-05-21 16:25:56 -0400

    Create another activity and landscape layout

---
+++ GEOQuiz/app/src/main/java/edu/wmich/cs/geoquiz/Question.java
@@ -0,0 +1,28 @@
+package edu.wmich.cs.geoquiz;
+
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+        mTextResId = textResId;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
