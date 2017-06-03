5e989fa42887720368fa180860b7c9a1df267251
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-06-02 23:13:35 -0400

    Add FlickrFetcher

--- PhotoGallery/app/src/main/java/edu/wmich/cs/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/edu/wmich/cs/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,21 @@
 package edu.wmich.cs.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment {
     private RecyclerView mPhotoRecyclerView;
+    private static final String TAG = "PhotoGalleryFragment";
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -20,6 +25,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -33,4 +39,17 @@ public class PhotoGalleryFragment extends Fragment {
         return  v;
     }
 
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void...params) {
+            try {
+                String result = new FlickrFetcher().getUrlString("https://www.bignerdranch.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Fetched content url: ", ioe);
+            }
+            return null;
+        }
+    }
 }
+
